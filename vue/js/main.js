688d75c908004b704785164a8f1bfbaa216aee43
Kristina <kristina.tibeykina04@mail.ru>
2024-02-01 15:47:46 +0700

    Условие перехода по столбцам и сам переход

--- vue/js/main.js
+++ vue/js/main.js
@@ -40,18 +40,61 @@ const app = new Vue({
         saveCards() {
             const cards = {
                 column1: this.column1,
+                column2: this.column2,
+                column3: this.column3
             };
 
             localStorage.setItem('cards', JSON.stringify(cards));
+        },
+        updateCardStatus() {
+            this.column1.forEach(card => {
+                const completedItems = card.items.filter(item => item.completed).length;
+                const completionPercentage = (completedItems / card.items.length) * 100;
+
+                if (completionPercentage >= 50) {
+                    this.column2.push(card);
+                    this.column1 = this.column1.filter(c => c.id !== card.id);
+                }
+
+            });
+
+            this.column2.forEach(card => {
+                const completedItems = card.items.filter(item => item.completed).length;
+                const completionPercentage = (completedItems / card.items.length) * 100;
+
+                if (completionPercentage === 100) {
+                    card.completedItems = completedItems;
+                    this.column3.push(card);
+                    this.column2 = this.column2.filter(c => c.id !== card.id);
+                }
+            });
+
+            this.saveCards();
         },
         loadCards() {
             const savedCards = JSON.parse(localStorage.getItem('cards'));
 
             if (savedCards) {
                 this.column1 = savedCards.column1 || [];
+                this.column2 = savedCards.column2 || [];
+                this.column3 = savedCards.column3 || [];
             }
         },
 
     },
+    watch: {
+        'column1': {
+            handler() {
+                this.updateCardStatus();
+            },
+            deep: true
+        },
+        'column2': {
+            handler() {
+                this.updateCardStatus();
+            },
+            deep: true
+        }
+    }
 
 })
